c58f288b550aaede17530f01e3b85496f2e5bd62
nachitoderacing <166016638+nachitoderacing@users.noreply.github.com>
2024-04-07 10:11:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let num1 = 4
+let num2 = 2
+let suma = num1 + num2
+let resta = num1 - num2
+let multiplicacion = num1 * num2
+let divición =  num1 / num2
+let año = 2024
+let edadalumno = 1990 
+let añoactual = prompt ("ingrese el año en el que nacio")
+let nombrealunmo = prompt('Ingresa Tu Nombre')
+let apellidoalunmo = prompt('Ingresa Tu Apellido')
+let diez = 10
+let tres = 3
+let resultadomultiplicacion = (diez + tres)
+alert (resultadomultiplicacion)
+alert ("bienvenido" + (nombrealunmo) + (apellidoalunmo));
+alert ("el valor de la variable es" + (suma))
+alert ("el valor de la variable es" + (resta))
+alert ("el valor de la variable es" + (multiplicacion))
+alert ("el valor de la variable es" + (divición))
+alert ("mi edad es" + (año - edadalumno))
+console.log ("la suma es" + (num1 + num2))
+console.log ("la resta es" + (num1 - num2))
+console.log ("la multiplicación es" + (num1 * num2))
+console.log ("la divición es" + (num1 / num2))
+console.log ("bienvenido" + (nombrealunmo) + (apellidoalunmo))
+let num3 = 10
+let num4 = 3
+let resultadodelamultiplicacion = num4 * num3
+alert = (resultadodelamultiplicacion)
